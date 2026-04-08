85c12ba558faec7a7dd7178ddfe625ac28122a7a
fasua samya <perfectseasongloballtd@gmail.com>
2026-04-08 01:36:53 +0100

    run client and server together

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import concurrently from 'concurrently':
+
+concurrently([
+   { 
+    name: 'server',
+    command: 'npm run dev',
+    cwd: 'packages/server',
+    prefixColor: 'cyan',
+   },
+
+   {
+    name: 'client',
+    command: 'npm run dev',
+    cwd: 'packages/client',
+    prefixColor: 'green',
+   }
+]);
